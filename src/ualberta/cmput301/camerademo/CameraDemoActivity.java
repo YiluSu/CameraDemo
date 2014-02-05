1c7d038b1192d4ac3a42354535967c963ec1a1a3
YiluSu <yilu@ualberta.ca>
2014-02-04 19:12:07 -0700

    CameraDemo

--- src/ualberta/cmput301/camerademo/CameraDemoActivity.java
+++ src/ualberta/cmput301/camerademo/CameraDemoActivity.java
@@ -1,9 +1,15 @@
 package ualberta.cmput301.camerademo;
 
+import java.io.File;
+
 import ualberta.cmput301.camerodemo.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,46 +18,61 @@ import android.widget.TextView;
 
 public class CameraDemoActivity extends Activity {
 
-	private TextView textView;
-	private ImageButton imageButton;
-	private Uri imageFileUri;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_camero_demo);
-		
-		// Retrieve handlers
-		textView = (TextView) findViewById(R.id.status);
-		imageButton = (ImageButton) findViewById(R.id.image);
-		
-		// Set up the listener
-		OnClickListener listener = new OnClickListener() {
-			public void onClick(View view) {
-				takeAPhoto(); // implement this method
-			}
-		};
-		// Register a callback to be invoked when this view is clicked
-		imageButton.setOnClickListener(listener);
-	}
+    private TextView textView;
+    private ImageButton imageButton;
+    private Uri imageFileUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camero_demo);
+
+        // Retrieve handlers
+        textView = (TextView) findViewById(R.id.status);
+        imageButton = (ImageButton) findViewById(R.id.image);
+
+        // Set up the listener
+        OnClickListener listener = new OnClickListener() {
+            public void onClick(View view) {
+                takeAPhoto(); // implement this method
+            }
+        };
+        // Register a callback to be invoked when this view is clicked
+        imageButton.setOnClickListener(listener);
+    }
 
 
-	// Implement takeAPhoto() method to allow you to take a photo when you click the ImageButton.
-	// Notice that startActivity() method will not return any result when the launched activity 
-	// finishes, while startActivityForResult() method will. To retrieve the returned result, you may 
-	// need implement onAcitityResult() method.
-	public void takeAPhoto() {
-		// To Do		
-	}
-	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// To Do
-	}	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.camero_demo, menu);
-		return true;
-	}
+    // Implement takeAPhoto() method to allow you to take a photo when you click the ImageButton.
+    // Notice that startActivity() method will not return any result when the launched activity 
+    // finishes, while startActivityForResult() method will. To retrieve the returned result, you may 
+    // need implement onAcitityResult() method.
+    public void takeAPhoto() {
+        // To Do
+        Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+//        File file = new File("/photo storage/file.jpeg");
+        String pathdir = Environment.getExternalStorageDirectory().getAbsolutePath() ;
+        Uri uri = Uri.parse(pathdir);
+        intent.putExtra("dir", uri);
+        startActivityForResult(intent,0);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // To Do
+        if (data != null) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bm = data.getExtras().getParcelable("data");
+                imageButton.setImageBitmap(bm);
+                textView.setText("photo ok");
+            } else if (resultCode == RESULT_CANCELED) {
+                textView.setText("Not sure what happende.");
+            }
+        }
+    }	
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.camero_demo, menu);
+        return true;
+    }
 
 }
